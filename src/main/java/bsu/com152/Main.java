a649c070e13c1edbeab10706701080472f42135e
Alan <alacerda@student.bridgew.edu>
2019-10-30 12:00:12 -0400

    creating buttons and colors

--- src/main/java/bsu/com152/Main.java
+++ src/main/java/bsu/com152/Main.java
@@ -2,7 +2,11 @@ package bsu.com152;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,9 +17,22 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        var text = new Label("Hello Comp152");
-        var container = new Scene(text);
+        VBox box = new VBox();
+        var text = new Label(" Hello Comp152");
+        var color = Paint.valueOf("darkgreen");
+        text.setTextFill(color);
+        var font = Font.font(22);
+        text.setFont(font);
+        box.getChildren().add(text);
+        Button pushButton  = new Button("Push Me");
+        pushButton.setFont(font);
+        var color2 = Paint.valueOf("Red");
+        pushButton.setTextFill(color2);
+        box.getChildren().add(pushButton);
+        var container = new Scene(box);
         primaryStage.setScene(container);
+        primaryStage.setTitle("  Example Window");
+        primaryStage.setMinWidth(300);
         primaryStage.show();
 
     }
